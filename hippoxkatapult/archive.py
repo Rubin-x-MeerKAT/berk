2d2d8a06f4697236e54a64bb4759e1b425fa962e
Matt Hilton <matt.hilton@mykolab.com>
2022-07-26 10:40:03 +0200

    Unpacking step via screen

--- hippoxkatapult/archive.py
+++ hippoxkatapult/archive.py
@@ -11,15 +11,16 @@ from . import jobs
 #------------------------------------------------------------------------------------------------------------
 def fetchFromArchive(captureBlockId):
     """Dummy routine (for now). Fetch the requested MeerKAT measurement set, identified using
-    captureBlockId (the equivalent of e.g. XMM ObsID), as a tar.gz. Return the path to the .tar.gz.
+    captureBlockId (the equivalent of e.g. XMM ObsID), as a tar.gz.
+
+    Returns:
+        None
 
     """
 
     pathToTGZ=config['stagingDir']+os.path.sep+"%s_sdp_l0.ms.tar.gz" % (captureBlockId)
     print("archive.fetchFromArchive - dummy retrieve - %s" % (pathToTGZ))
     assert(os.path.exists(pathToTGZ))
-
-    return pathToTGZ
 
 #------------------------------------------------------------------------------------------------------------
 def stageMS(captureBlockId):
@@ -31,12 +32,18 @@ def stageMS(captureBlockId):
 
     """
 
+    # NOTE: MOVE THIS INTO HIPPOXKATAPULT_UNPACK
     MSPath=config["stagingDir"]+os.path.sep+captureBlockId+"_sdp_l0.ms"
     if os.path.exists(MSPath) == False:
         pathToTGZ=fetchFromArchive(captureBlockId)
         topDir=os.getcwd()
         os.chdir(config['stagingDir'])
-        jobID=jobs.submitJob("tar -zxvf %s" % (pathToTGZ), "unpack")
+        #jobID=jobs.submitJob("tar -zxvf %s" % (pathToTGZ), "unpack")
+        for p in os.walk("scratch"):
+            if p[1][0] == os.path.split(MSPath)[-1]:
+                break
+        # NOTE: We'd need a job to do this
+        mvCmd="mv %s/%s ." % (p[0], p[1][0])
         os.chdir(topDir)
     assert(os.path.exists(MSPath))
 

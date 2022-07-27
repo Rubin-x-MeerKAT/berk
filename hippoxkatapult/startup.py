8edecdeef3bfbfdb914f1a3099a0ea963c3974d1
Matt Hilton <matt.hilton@mykolab.com>
2022-07-27 14:12:28 +0200

    Added other dirs, oxkat fetching

--- hippoxkatapult/startup.py
+++ hippoxkatapult/startup.py
@@ -16,15 +16,22 @@ config['rootDir']="/data/mjh/MeerKAT/MSS"
 config['stagingDir']=config['rootDir']+os.path.sep+"staging"
 # stagingDir="/scratch/month/mjh/"
 
+# Directory where we do processing
+config['processingDir']=config['rootDir']+os.path.sep+"processing"
+
+# Directory where we archive data products
+config['productsDir']=config['rootDir']+os.path.sep+"products"
+
 # Directory where we'll cache e.g. oxkat
-config['cacheDir']="/data/mjh/hippoxkatapult-cache"
+config['cacheDir']=config['rootDir']+os.path.sep+"cache"
 
 # Oxkat version to use
 config['oxkatVersion']="0.3"
 config['oxkatDir']=config['cacheDir']+os.path.sep+"oxkat-%s" % (config['oxkatVersion'])
+config['oxkatURL']="https://github.com/IanHeywood/oxkat/archive/refs/tags/v%s.tar.gz" % (config['oxkatVersion'])
 
 # Set-up ----------------------------------------------------------------------------------------------------
-dirsToMake=[config['stagingDir'], config['cacheDir'], config['oxkatDir']]
+dirsToMake=[config['stagingDir'], config['processingDir'], config['productsDir'], config['cacheDir']]
 for d in dirsToMake:
     os.makedirs(d, exist_ok = True)
 
@@ -32,5 +39,5 @@ if os.path.exists(config['oxkatDir']) == False:
     topDir=os.getcwd()
     os.chdir(config['cacheDir'])
     os.system("wget %s" % (config['oxkatURL']))
-    os.system("tar -zxvf v%.s.tar.gz" % (config['oxkatVersion']))
+    os.system("tar -zxvf v%s.tar.gz" % (config['oxkatVersion']))
     os.chdir(topDir)

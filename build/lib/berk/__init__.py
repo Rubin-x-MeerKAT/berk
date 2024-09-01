755964c7bf7eba5df82172a7c746025ac1b2315e
Unnikrishnan Sureshkumar <usureshkumar@login1.cm.cluster>
2024-09-01 18:32:02 +0200

    Log file during fetch

---
+++ build/lib/berk/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+Berk - a package for producing and managing MeerKAT continuum survey data.
+
+"""
+
+#from . import catalogs 
+from . import startup
+
+#__all__ = ['startUp']
+
+__version__ = 0.2
